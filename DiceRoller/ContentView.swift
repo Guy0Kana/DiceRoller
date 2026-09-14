22aa74be9c988ed84fb0216dcd555cdbbcae4b21
Guyo <guyogodana005@gmail.com>
2026-09-14 18:22:29 +0300

    Initial Commit

---
+++ DiceRoller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceRoller
+//
+//  Created by Guyo Godana on 14/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Guyo Godana on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
